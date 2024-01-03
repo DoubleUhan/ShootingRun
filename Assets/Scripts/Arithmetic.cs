50e56d4dd3c80d2d30e83c141fe82f66d7827fa0
Junyuhan04 <junyuhan04@gmail.com>
2024-01-04 03:54:57 +0900

    Feat/CopyPlayer

---
+++ Assets/Scripts/Arithmetic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arithmetic : MonoBehaviour
+{
+    float moveSpeed = 5;
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        // 현재 위치를 가져오고 x축으로 이동
+        Vector3 newPosition = transform.position + new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+        // 새로운 위치로 이동
+        transform.position = newPosition;
+    }
+
+}
