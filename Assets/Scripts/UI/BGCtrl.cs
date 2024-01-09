7f0ee09564a88aac3e18b4b90a168c94a265047d
junyuhan <junyuhan04@gmail.com>
2024-01-09 19:57:54 +0900

    feat/AssetsApply

--- Assets/Scripts/UI/BGCtrl.cs
+++ Assets/Scripts/UI/BGCtrl.cs
@@ -1,49 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BGCtrl : MonoBehaviour
 {
-    public GameObject groundPrefab; // 생성할 지형 프리팹
+    Queue<Transform> transforms = new Queue<Transform>();
+
     public int numberOfGrounds = 4; // 동시에 표시될 지형 개수
 
-    private List<GameObject> grounds = new List<GameObject>();
-    private float groundLength; // 지형의 길이
-    private float spawnZ = 0f; // 다음 지형을 생성할 Z 위치
-    private float safeZone = 100f; // 플레이어가 멈출 때 새로운 지형을 생성하는 위치
+    public Transform[] grounds;
+    public Transform endPos;
+    public Transform startPos;
 
     void Start()
     {
-        // 지형 프리팹의 길이를 측정
-        groundLength = groundPrefab.GetComponent<Renderer>().bounds.size.z;
-
-        // 초기 지형 생성
-        for (int i = 0; i < numberOfGrounds; i++)
+        foreach (Transform ground in grounds)
         {
-            SpawnGround();
+            transforms.Enqueue(ground);
         }
     }
 
     void Update()
     {
-        // 플레이어가 일정 위치에 도달하면 새로운 지형을 생성
-        if (transform.position.z - safeZone > (spawnZ - numberOfGrounds * groundLength))
+        if (transforms.Count <= 0)
+            return;
+
+        if (Vector3.Distance(transforms.Peek().position, endPos.position) <= 0.5f)
         {
-            SpawnGround();
-            DeleteGround();
+            MoveBack(transforms.Dequeue());
         }
+
     }
 
-    void SpawnGround()
+    void MoveBack(Transform target)
     {
-        GameObject ground = Instantiate(groundPrefab, new Vector3(0, 0, spawnZ), Quaternion.identity);
-        spawnZ += groundLength;
-        grounds.Add(ground);
+        // 이동
+        target.position = startPos.position;
+
+        transforms.Enqueue(target);
     }
 
-    void DeleteGround()
+    void Move()
     {
-        Destroy(grounds[0]);
-        grounds.RemoveAt(0);
+
     }
+
 }
