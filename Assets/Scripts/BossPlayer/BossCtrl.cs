c44dffcf8dda45721c157c736ede4498f1b28cf5
yong <lyw3059@gmail.com>
2024-01-10 16:27:49 +0900

    feat: bossanimation

--- Assets/Scripts/BossPlayer/BossCtrl.cs
+++ Assets/Scripts/BossPlayer/BossCtrl.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Animations.Rigging;
 using UnityEngine.UI;
 
 public class BossCtrl : Stats
@@ -10,6 +14,7 @@ public class BossCtrl : Stats
     public float delay;
 
     public GameObject target; // 바라볼 타겟이랑 겹치느ㅜ듯
+    public GameObject formatTarget; // 다시 가운데 보게하는 타겟
 
     public GameObject warning; // 빨간 예고 범위
 
@@ -21,42 +26,77 @@ public class BossCtrl : Stats
     [SerializeField] float maxBossHP;
     [SerializeField] float curBossHP; // 보스 체력 설정
 
+    public GameObject trackingTarget;
+
     Animator animator;
     public static bool isSkillActive;
+    public static bool warningOn = false;
     bool isDead; // 보스 죽었는지 안죽었는지 체크하는 변수
 
     [Range(0, 10)]
     public float attackArrange;
 
+    MultiAimConstraint multiAimConstraint;
+    RigBuilder rigBuilder;
+
     void Start()
     {
+        multiAimConstraint = trackingTarget.GetComponent<MultiAimConstraint>();
         SoundManager.Instance.Boss_BGM();
         target = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(BossAttack());
         delay = 3.0f;
-        animator = GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
+        rigBuilder = GetComponent<RigBuilder>();
     }
+
     void Update()
     {
         Debug.Log(Time.timeScale);
-        if (isSkillActive == false)
+        if (warningOn == true)
         {
-            Vector3 dir = target.transform.position - transform.position;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10);
-            
-            // 뭔지 이제 모름
-            //transform.LookAt(target.transform);
-            // var a = transform.rotation;
-            //a = new Quaternion(0, transform.rotation.y, 0,0);
-            //Debug.Log("a "+ transform.rotation);
-            //transform.localRotation = new Quaternion(0, transform.rotation.y, 0, 0);
-            //Debug.Log(transform.rotation);
+            Vector3 warningTarget = warning.transform.position;
+            warningTarget.y = transform.position.y - 5;
+            Vector3 dir = warningTarget - transform.position;
+            if (multiAimConstraint.data.sourceObjects.GetWeight(0).Equals(1))
+            {
+                var a = multiAimConstraint.data.sourceObjects;
+                a.SetWeight(0, 0);
+                a.SetWeight(1, 1);
+                multiAimConstraint.data.sourceObjects = a;
+            }
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 5);
         }
-        else
+        else if (warningOn == false)
         {
-            Vector3 dir = warning.transform.position - transform.position;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10);
+            if (multiAimConstraint.data.sourceObjects.GetWeight(0).Equals(0))
+            {
+                var a = multiAimConstraint.data.sourceObjects;
+                a.SetWeight(0, 1);
+                a.SetWeight(1, 0);
+                multiAimConstraint.data.sourceObjects = a;
+            }
+            Vector3 dir = formatTarget.transform.position - transform.position;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 3);
         }
+
+        //if (isSkillActive == false)
+        //{
+        //    Vector3 dir = target.transform.position - transform.position;
+        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10);
+
+        //    // 뭔지 이제 모름
+        //    //transform.LookAt(target.transform);
+        //    // var a = transform.rotation;
+        //    //a = new Quaternion(0, transform.rotation.y, 0,0);
+        //    //Debug.Log("a "+ transform.rotation);
+        //    //transform.localRotation = new Quaternion(0, transform.rotation.y, 0, 0);
+        //    //Debug.Log(transform.rotation);
+        //}
+        //else
+        //{
+        //}
     }
 
 
@@ -64,14 +104,13 @@ public class BossCtrl : Stats
     IEnumerator BossAttack()
     {
         yield return new WaitForSeconds(3f);
-        int num = 0; // Random.Range(0, 3);
         while (!isDead)
         {
-            // Random.Range(0, 3);
+            int num = 0; // Random.Range(0, 3);
             switch (num)
             {
                 case 0: // 내려 찍기
-                    StartCoroutine(PullDown());
+                    yield return StartCoroutine(PullDown());
                     break;
 
                 case 1: // 휩쓸기
@@ -88,21 +127,28 @@ public class BossCtrl : Stats
 
     IEnumerator PullDown()
     {
+        warningOn = true;
         warning.transform.position = new Vector3(target.transform.position.x, 0, target.transform.position.z);
         warning.SetActive(true);
         isSkillActive = true;
 
         yield return new WaitForSeconds(1f);
+        warning.SetActive(false);
 
         animator.Play("ATK_pattern_1");
 
         if (warning.GetComponent<BossSkillRange>().isPlayerIn)
         {
             GameFail();
+
+
         }
-        warning.SetActive(false);
-        SoundManager.Instance.Boss_Smile(); // 사운드매니저에서 불러옴
+        SoundManager.Instance.Boss_Smile(); // 보스 웃음 소리
+
         Debug.Log("공격 소리 났다");
+        yield return new WaitForSeconds(1.33f); // 애니메이션 실행 시간
+        warningOn = false;
+
         yield return null;
     }
 
