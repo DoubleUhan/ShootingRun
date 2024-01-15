3ba0daf86925cc638d26614586e8e70306a07611
kiss8981 <kim@dohyun.kr>
2024-01-15 13:31:13 +0900

    feat/death particle

--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -5,17 +5,15 @@ using UnityEngine.AI;
 
 public class TestEnemy : Stats
 {
+    ParticleSystem deathParticle;
     NavMeshAgent agent;
     public Transform player;
-
-    void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
 
     void Start()
     {
         player = FindObjectOfType<PlayCtrl>().transform;
+        deathParticle = GetComponentInChildren<ParticleSystem>();
+        agent = GetComponent<NavMeshAgent>();
     }
     void Update()
     {
@@ -27,15 +25,20 @@ public class TestEnemy : Stats
         // 길찾기 시작
         agent.isStopped = false;
     }
-
+   /* private void OnDestroy()
+    {
+        
+        Destroy(this);
+    }*/
     public void OnDamaged(float Damage)
     {
         HP -= Damage;
         if (HP <= 0)
         {
+            deathParticle.transform.parent = null;
+            deathParticle.Play();
             Destroy(gameObject);
-            GameManager.Instance.enemyCount++;
-            GameManager.Instance.ClearStage();
+            
         }
     }
 }
