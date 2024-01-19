453c8f2d129c87a27a0fb9d78e6034a84810f4f7
junyuhan <junyuhan04@gmail.com>
2024-01-19 20:14:47 +0900

    feat/helptlqkf

--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -8,16 +8,18 @@ public class StageManager : SingletonMonoBase<StageManager>
     public bool[] isClear_Stage = new bool[3];
     public Vector3[] stagePos =
     {
-        new Vector3(-19.75f, 1.16709f, 88.6f),
-        new Vector3(-31, 0, 72),
-        new Vector3(-29, 0, 41),
+        new Vector3(-19.75f, 0, 88.6f),
+        new Vector3(-46.1f, 1.21f, 17.9f),
+        new Vector3(2.02f, 0, -6.6f),
+       // new Vector3(-29, 0, 41),
     };
-   
+
     public int clearStageMax = 0;
     public Transform stageCol;
 
     public Vector3 GetStartPos()
     {
+        Debug.Log(stagePos[clearStageMax]);
         return stagePos[clearStageMax];
     }
 
@@ -26,6 +28,7 @@ public class StageManager : SingletonMonoBase<StageManager>
         if (clearStageMax + 1 < stagePos.Length)
         {
             stageCol.position = stagePos[clearStageMax + 1];
+            Debug.Log(stageCol.position);
         }
         else
         {
